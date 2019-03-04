bff81ef6a633931c492490512ec7676fc010e080
gulfa <gunnarroe@gmail.com>
2019-03-04 13:54:42 +0200

    Configuring licences and restricted schema

--- ckanext/unaids/plugin.py
+++ ckanext/unaids/plugin.py
@@ -1,12 +1,27 @@
 import ckan.plugins as p
 import logging
- 
+import licenses
+import ckan.model.license as core_licenses
+import ckan.model.package as package
+
 log = logging.getLogger(__name__)
+
+
+def add_licenses():
+    package.Package._license_register = core_licenses.LicenseRegister()
+    package.Package._license_register.licenses = [
+        core_licenses.License(
+            licenses.LicenseCreativeCommonsIntergovernmentalOrgs()),
+        core_licenses.License(
+            core_licenses.LicenseNotSpecified())
+        ]
+
+    
 
 
 class UNAIDSPlugin(p.SingletonPlugin):
     """
-    This plugin implements a way to connect superset to a table in the datastore
+    This plugin implements the configurations needed for AIDS data exchange
 
     """
     
@@ -17,6 +32,7 @@ class UNAIDSPlugin(p.SingletonPlugin):
         '''
         This method allows to access and modify the CKAN configuration object
         '''
+        add_licenses()
         log.info("UNAIDS Plugin is enabled")
         p.toolkit.add_template_directory(config, 'theme/templates')
         p.toolkit.add_public_directory(config, 'theme/public')
